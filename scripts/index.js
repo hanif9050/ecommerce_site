50458a07310a8d0c89875a6880af4bbef78134ec
Mohammed Hanif <hanif.ctg9050@gmail.com>
2023-05-22 12:36:27 +0600

    nav-toggle added

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+const navIcon=document.getElementById("menu-icon");
+const menu=document.getElementById("menu");
+
+navIcon.addEventListener("click",()=>{
+    
+    menu.classList.toggle("hidden")
+})
